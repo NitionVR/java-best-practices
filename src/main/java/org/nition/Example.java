6f01bdcaa93987e77263c1a3f55910f3ea41d6e9
NitionVR <kmaganyane83@gmail.com>
2024-06-27 10:06:54 +0200

    Testing for CodeQL fail checks

--- src/main/java/org/nition/Example.java
+++ src/main/java/org/nition/Example.java
@@ -3,9 +3,15 @@ package org.nition;
 public class Example {
     public static void main(String[] args) {
         System.out.println(greeting());
+        System.out.println(getDatabasePassword());
     }
 
     public static String greeting(){
         return "Hello World!";
+    }
+
+
+    public static String getDatabasePassword() {
+        return "superSecretPassword";
     }
 }
--- src/test/java/org/nition/TestExample.java
+++ src/test/java/org/nition/TestExample.java
@@ -1,17 +1,19 @@
 package org.nition;
 
 import org.junit.jupiter.api.Test;
+import org.nition.Example;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestExample {
 
-
     @Test
     void testGreeting(){
-
-        assertEquals(Example.greeting(),"Hello World!");
+        assertEquals(Example.greeting(), "Hello World!");
     }
 
+    @Test
+    void testGetDatabasePassword(){
+        assertEquals(Example.getDatabasePassword(), "superSecretPassword");
+    }
 }
